9a9274205af4790604a53d7cefd495a4de71d5cf
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmal.com>
2019-12-19 15:01:07 +0200

    First initilization

---
+++ src/Structuring.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SaatyMethod;
+
+/**
+ * Class contains methods for structuring the selection problem 
+ * as a hierarchy or network.
+ *
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+class Structuring
+{
+    //put your code here
+}
---
+++ src/StructuringInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SaatyMethod;
+
+/**
+ * StructuringInterface is an interface that defines the first step in applying
+ * the hierarchy analysis method - structuring the selection problem in 
+ * the form of a hierarchy or network. In the most elementary form, 
+ * the hierarchy is built from the top (goal), through intermediate 
+ * levels - criteria to the lowest level, which in the general case
+ * is a set of alternatives.
+ * 
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+interface StructuringInterface
+{
+    //put your code here
+}
