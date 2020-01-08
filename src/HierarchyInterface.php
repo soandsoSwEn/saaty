fa3e7fc9b5a28ff6818243fd8b9087afc23234cd
Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
2020-01-08 14:59:16 +0200

    Add HierarchyInterface

---
+++ src/HierarchyInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SaatyMethod;
+
+/**
+ * HierarchyInterface is an interface that contains methods for evaluating
+ * each alternative by criteria and obtaining a final vector of priorities,
+ * based on which the best of alternatives is determined
+ * 
+ * @author Dmytriyenko Vyacheslav <dmytriyenko.vyacheslav@gmail.com>
+ */
+interface HierarchyInterface
+{
+    public function setStructureData(Structuring $data) : void;
+    
+    public function getStructureData() : object;
+    
+    public function setSummaryData(array $pairwise_comparisons) : void;
+    
+    public function getSummaryData() : array;
+    
+    public function getBestAlternative(array $data) : string;
+}
